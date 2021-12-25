1f54f538936c119b1a0bc270e42d9ca2ec5aa2aa
IlyaL <@DESKTOP-L3K3R15>
2021-12-25 13:26:16 +0200

    HW_23 IlyaL commit to review

--- src/telran/util/time/DateTimeTests.java
+++ src/telran/util/time/DateTimeTests.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.Test;
 class DateTimeTests {
 	LocalDate birthAS = LocalDate.of(1799, 6, 6);
 	LocalDate barMizvaAS = birthAS.plusYears(13);
+	static final int REITERATE=1000000;
 	@BeforeEach
 	void setUp() throws Exception {
 	}
@@ -23,17 +24,23 @@ class DateTimeTests {
 		assertEquals("1812-06-06", barMizvaAS.toString());
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("d/M/yyyy");
 		assertEquals("6/6/1812", barMizvaAS.format(dtf));
-		System.out.println(barMizvaAS.format(DateTimeFormatter.ofPattern("d MMM E yyyy ")));
+		//System.out.println(barMizvaAS.format(DateTimeFormatter.ofPattern("d MMM E yyyy ")));
 		assertEquals(LocalDate.parse("1812-06-06"), barMizvaAS);
 		assertEquals(13 * 12, ChronoUnit.MONTHS.between(birthAS, barMizvaAS));
 	}
 	@Test
 	void testBarMizvaAdjuster() {
 		assertEquals(barMizvaAS, birthAS.with(new BarMizvaAdjuster()));
-	}
+	} 
 	@Test
 	void testNextFriday13Adjuster() {
+		for (int i=1;i<REITERATE;i++) {
 		assertEquals(LocalDate.parse("2022-05-13"), LocalDate.parse("2021-12-22").with(new NextFriday13Adjuster()));
+		assertEquals(LocalDate.parse("2022-05-13"), LocalDate.parse("2022-01-13").with(new NextFriday13Adjuster()));
+		assertEquals(LocalDate.parse("2021-08-13"), LocalDate.parse("2021-07-16").with(new NextFriday13Adjuster()));
+		assertEquals(LocalDate.parse("1994-05-13"), LocalDate.parse("1993-11-04").with(new NextFriday13Adjuster()));
+		assertEquals(LocalDate.parse("1952-06-13"), LocalDate.parse("1952-02-27").with(new NextFriday13Adjuster()));
+		}
 	}
 	@Test
 	void testGetAge() {
@@ -43,12 +50,16 @@ class DateTimeTests {
 		assertEquals(2021 - 1799, getAgeAtDate(birthAS, ld1));
 		assertEquals(2020 - 1799, getAgeAtDate(birthAS, ld2));
 		assertEquals(2020 - 1799, getAgeAtDate(birthAS, ld3));
+		LocalDate birth = LocalDate.parse("1936-03-24");
+		LocalDate death = LocalDate.parse("2007-03-24");
+		assertEquals(71, getAgeAtDate(birth, death));
+
 	}
 
 	private int getAgeAtDate(LocalDate birthDate, LocalDate atDate) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+		
+		return (int) ChronoUnit.YEARS.between(birthDate, atDate);
+	} 
 	
 
 }
--- src/telran/util/time/NextFriday13Adjuster.java
+++ src/telran/util/time/NextFriday13Adjuster.java
@@ -1,14 +1,44 @@
 package telran.util.time;
-//HW_23
+//HW_23 
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
 import java.time.temporal.Temporal;
 import java.time.temporal.TemporalAdjuster;
 
 public class NextFriday13Adjuster implements TemporalAdjuster {
-
+	private static final int DAY13=13;
+	
 	@Override
+	
+/*	
+ // VARIANT 1, watched at the webinar, on a million repetitions it works 4 seconds
 	public Temporal adjustInto(Temporal temporal) {
-		// TODO Auto-generated method stub
-		return null;
+		temporal=getNearest13Day(temporal);
+		while(DayOfWeek.from(temporal) != DayOfWeek.FRIDAY){
+			temporal=temporal.plus(1, ChronoUnit.MONTHS);
+		}
+		return temporal;
 	}
+	// get nearest date with DAY_OF_MOTNTH equals 13
+	private Temporal getNearest13Day(Temporal temporal) {
+		if(temporal.get(ChronoField.DAY_OF_MONTH) >= 13) {
+			temporal = temporal.plus(1, ChronoUnit.MONTHS);
+		}
+		temporal = temporal.with(ChronoField.DAY_OF_MONTH, 13);
+		return temporal;
+	}
+*/
+
+ // VARIANT 2, I wrote it with a minimum of methods, 7 seconds work on a million repetitions
+	public Temporal adjustInto(Temporal temporal) {
+		LocalDate date = LocalDate.from(temporal);
+		while ((date.getDayOfMonth() != DAY13) ||
+		(date.getDayOfWeek() != DayOfWeek.FRIDAY)) date=date.plusDays(1);
+	return temporal.with(date);
+	}
+
 
 }
